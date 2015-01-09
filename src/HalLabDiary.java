12afaf25686f32078b94e4c2eb7729f72119a08c
U-MA <g.0224incident@gmail.com>
2015-01-09 10:24:09 +0900

    不要なimportの削除

--- src/HalLabDiary.java
+++ src/HalLabDiary.java
@@ -1,10 +1,3 @@
-import org.htmlparser.Node;
-import org.htmlparser.Parser;
-import org.htmlparser.filters.HasAttributeFilter;
-import org.htmlparser.util.EncodingChangeException;
-import org.htmlparser.util.NodeList;
-import org.htmlparser.util.ParserException;
-
 public class HalLabDiary {
 
     private static final String DIARYURL = "../../Downloads/hallab.html";
